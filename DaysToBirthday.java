2d1fb3c9fad6ba6ac97c584ad7cadb35b18efc28
aschnu <aschnu@o2.pl>
2017-09-07 14:25:17 +0200

    Update DaysToBirthday.java

--- DaysToBirthday.java
+++ DaysToBirthday.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication2;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -13,7 +6,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author AGRZEGOR
+ * @author aschnu
  */
 public class DaysToBirthday {
 
